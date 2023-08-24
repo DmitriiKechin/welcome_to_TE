420eb55d63250300604ba88a91bd56a03445bc0d
Dmitrii Kechin <dmitrii.kechin@gmail.com>
2023-08-24 16:32:09 +0300

    completed the first task

--- task_1/index.tsx
+++ task_1/index.tsx
@@ -1,46 +1,58 @@
-import { Component } from 'react';
+import { Component, PureComponent, memo } from 'react';
 
 type IUser = {
-    name: string
-    age: number
-}
+  name: string;
+  age: number;
+};
 
 type IProps = {
-    user: IUser
-}
+  user: IUser;
+};
 
 // functional component
-const FirstComponent = ({ name, age }: IUser) => (
-    <div>
-        my name is {name}, my age is {age}
-    </div>
-);
+const FirstComponent = memo(({ name, age }: IUser) => (
+  <div>
+    my name is {name}, my age is {age}
+  </div>
+));
 
 // functional component
-const SecondComponent = ({ user: { name, age } }: IProps) => (
+const SecondComponent = memo(
+  ({ user: { name, age } }: IProps) => (
     <div>
-        my name is {name}, my age is {age}
+      my name is {name}, my age is {age}
     </div>
+  ),
+  (prevProps, nextProps) => {
+    if (prevProps.user === nextProps) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 );
 
 // class component
-class ThirdComponent extends Component<IUser> {
-    render() {
-        return (
-            <div>
-                my name is {this.props.name}, my age is {this.props.age}
-            </div>
-        )
-    }
+class ThirdComponent extends PureComponent<IUser> {
+  render() {
+    return (
+      <div>
+        my name is {this.props.name}, my age is {this.props.age}
+      </div>
+    );
+  }
 }
 
 // class component
 class FourthComponent extends Component<IProps> {
-    render() {
-        return (
-            <div>
-                my name is {this.props.user.name}, my age is {this.props.user.age}
-            </div>
-        )
-    }
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.user !== this.props.user;
+  }
+  render() {
+    return (
+      <div>
+        my name is {this.props.user.name}, my age is {this.props.user.age}
+      </div>
+    );
+  }
 }
